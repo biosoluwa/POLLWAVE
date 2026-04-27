e6d1c7e67319e1e259092d60ac73e31982da6b7b
biosoluwa <favbiose@gmail.com>
2026-04-27 01:12:42 +0100

    added post request and created server.js

---
+++ public/create.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault()
+    const pollFormData = new FormData(form)
+    const question = pollFormData.get('question')
+    const option1 = pollFormData.get('option1')
+    const option2 = pollFormData.get('option2')
+    const option3 = pollFormData.get('option3')
+    const option4 = pollFormData.get('option4')
+
+if(question, option1, option2, option3, option4){
+    const pollObj = {
+        question: question,
+        option1: option1,
+        option2: option2,
+        option3: option3,
+        option4: option4,
+        id: crypto.randomUUID()
+    }
+}
+    try{
+    await fetch('/poll', {
+        method: "POST",
+        headers:{
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(pollObj)
+    })
+    }catch(err){
+        console.error('Post request failed', err.message)
+    }
+
+})
