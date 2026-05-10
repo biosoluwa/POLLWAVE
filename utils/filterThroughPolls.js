0b0d3dfd79c1c26be8ed9e6b411fda3d9c5c5e4c
biosoluwa <favbiose@gmail.com>
2026-05-10 14:11:19 +0100

    increment vote and write back to file

--- utils/filterThroughPolls.js
+++ utils/filterThroughPolls.js
@@ -2,14 +2,19 @@ import fs from "node:fs/promises"
 import path from 'node:path'
 
 export async function filterThroughPolls(voteData){
-    const filePath = path.join('data', 'data.json')
+const filePath = path.join('data', 'data.json')
+try{
     let allPolls = await fs.readFile(filePath)
     allPolls = JSON.parse(allPolls)
-console.log(typeof allPolls)
 let poll = allPolls.filter(poll =>poll.id === voteData.id)[0]
 const option = poll.options.find(function(optionObj){
     return optionObj.text === voteData.text
 })
 
  option.votes ++
+
+ await fs.writeFile(filePath, JSON.stringify(allPolls, null, 2), 'utf8')
+}catch(err){
+    console.error(err)
+}
 }
